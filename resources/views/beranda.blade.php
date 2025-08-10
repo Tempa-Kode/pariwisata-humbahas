27067c0260e98960a6796d8d115c39bfcd7b8532
Henry Jr Zai <hjuniorzai@gmail.com>
2025-08-10 23:30:55 +0700

    add : menampilkan seluruh data wisata dalam satu maps admin

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -3,5 +3,33 @@
 @section('title', 'Beranda')
 
 @section('konten')
-    halaman beranda
+    <div id="map" style="height: 500px;"></div>
+{{--    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />--}}
+{{--    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>--}}
+{{--    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>--}}
 @endsection
+
+@push('script')
+    <script>
+        var map = L.map('map').setView([2.288971175704209, 98.53564577695926], 10);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© Kabupaten Humbang Hasundutan',
+        }).addTo(map);
+
+        $.ajax({
+            url: '/api/wisata', // Make sure this route returns JSON data
+            method: 'GET',
+            success: function(data) {
+                console.log(data)
+                data.forEach(function(wisata) {
+                    if(wisata.latitude && wisata.longitude) {
+                        L.marker([wisata.latitude, wisata.longitude])
+                            .addTo(map)
+                            .bindPopup(wisata.nama_wisata);
+                    }
+                });
+            }
+        });
+    </script>
+@endpush
